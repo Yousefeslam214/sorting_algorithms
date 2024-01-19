ebba1e09193283d519374c1aa9257d87cdcae981
Yousef eslam <yousefeslam214@gmail.com>
2024-01-19 17:25:39 +0200

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,44 @@
 #include "sort.h"
 
-void swap (int *x, int *y)
+/**
+ * swap - function to swap two integers in an array.
+ * @x: first integer
+ * @y: second integer
+ */
+void swap(int *x, int *y)
 {
     int temp = *x;
     *x = *y;
     *y = temp;
 }
 
-
+/**
+ * bubble_sort - function to implement the bubble sort algorithm on an array.
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-    bool flag = true; size_t i = 0;size_t j = 0;
-    while( i < size - 1)
+    bool flag = true;
+    size_t i = 0, j = 0;
+
+    while (i < size - 1)
     {
         j = 0;
-        while( j < size - i - 1)
+        while (j < size - i - 1)
         {
             if (array[j] > array[j + 1])
             {
                 swap(&array[j], &array[j + 1]);
                 flag = false;
+                /* Print array after each swap */
                 print_array(array, size);
             }
             j++;
         }
         i++;
+
+        /* If no swaps occurred, array is already sorted */
         if (flag == true)
             break;
     }
