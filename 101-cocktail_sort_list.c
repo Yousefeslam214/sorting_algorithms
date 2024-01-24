01e021005a9f1bad1c2ed2b0d997a8bba181e882
AlaaBadawii <141270202+AlaaBadawii@users.noreply.github.com>
2024-01-24 20:00:19 +0200

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -8,39 +8,37 @@
  **/
 void cocktail_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL)
-        return;
+        listint_t *sorted, *current, *temp, *next;
+        if (list == NULL || *list == NULL)
+                return;
+
+        sorted = NULL;
+        current = *list;
     
-    listint_t *sorted = NULL;
-    listint_t *current = *list;
-    listint_t *temp, *next;
-    
-    while (current != NULL)
-    {
-        next = current->next;
-        if (sorted == NULL || sorted->n >= current->n)
+        while (current != NULL)
         {
-            current->next = sorted;
-            current->prev = NULL;
-            if(sorted != NULL)
-                sorted->prev = current;
-            sorted = current;
-            print_list(current);
+                next = current->next;
+                if (sorted == NULL || sorted->n >= current->n)
+                {
+                        current->next = sorted;
+                        current->prev = NULL;
+                        if(sorted != NULL)
+                                sorted->prev = current;
+                        sorted = current;
+                }
+                else
+                {
+                        temp = sorted;
+                        while(temp->next != NULL && temp->next->n < current->n)
+                                temp = temp->next;
+                        current->next = temp->next;
+                        current->prev = temp;
+                        if(temp->next != NULL)
+                                temp->next->prev = current;
+                        temp->next = current;
+                        print_list(current);
+                }
+                current = next;
         }
-        else
-        {
-            temp = sorted;
-            while(temp->next != NULL && temp->next->n < current->n)
-                temp = temp->next;
-            current->next = temp->next;
-            current->prev = temp;
-            if(temp->next != NULL)
-                temp->next->prev = current;
-            temp->next = current;
-            print_list(current);
-        }
-        current = next;
-        print_list(current);
-    }
-    *list = sorted;
+        *list = sorted;
 }
